d57018709d62c98fd1648e215cbd91070ac4a4a7
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-06-01 23:48:05 +0600

    added API to store user info upon registration

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const uri = `mongodb+srv://${process.env.FREELANERDB_USER}:${process.env.FREELANERDB_PASS}@cluster0.udgfocl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const userCollection = client.db("freelancerMarketplaceDB").collection("freelancerUser");
+
+    app.post('/addUser', async(req, res)=>{
+        const userInfo = req.body;
+        const result = await userCollection.insertOne(userInfo);
+
+        res.send(result);
+    })
+
+
+
+  } finally {}
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, ()=>{
